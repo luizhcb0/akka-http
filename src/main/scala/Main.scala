44981fee1001a9946128d27b215e730495b2e9ba
luizhcb0 <luizhcb0@gmail.com>
2018-11-06 21:12:00 -0200

    task 888339 - enriquecendo dados pelo mid

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,8 @@ import akka.http.scaladsl.server.Directives
 import akka.stream.ActorMaterializer
 import scalaj.http
 import spray.json._
+
+import scala.concurrent.Promise
 //import play.api.libs.json._
 import org.json4s._
 import org.json4s.native.JsonMethods._
@@ -28,6 +30,7 @@ object Main extends App with Directives with JsonSupport {
   val token = "AnyStringToken"
   val secret = "08829edb4d005149ba0935c62f78276a"
   val appId = "284511972397990"
+  val appPSID = "357600121333928"
   val pageAccessToken = "EAACUOu3AkJMBAGsN5ZBXaIK9LonwZCvCmLnZAMFtZA7xZCoNic2MjfGWORlJsrZAXJOZBU5tJr2dz3GxeHPpkzAUtWmFClcQkGsKWZCZAJaSVPUBu8WIiiqS25x8V5KAYkSWxyjMj44AZAlzAQl3rUb5Gm1Iq4HiltqoUDx1SlIRIxoQZDZD"
   case class TestAPIParams(name: String, phone: Int)
 
@@ -35,6 +38,55 @@ object Main extends App with Directives with JsonSupport {
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   implicit val executionContext = system.dispatcher
+
+  def getMessageInfo(mid: String): Unit = {
+    val p = Promise[String]()
+    val f = p.future
+    val uri = s"https://graph.facebook.com/${mid}?fields=from,id,to,message&access_token=${pageAccessToken}"
+    println(uri)
+    val producer = Future {
+      val response: scalaj.http.HttpResponse[String] = scalaj.http.Http(uri).asString
+      p success response.body
+    }
+    val consumer = Future {
+      f foreach { response =>
+        val js = parse(response)
+        println(response)
+        var str = "{"
+        str += "\"from\":\"" + (js \\ "from" \ "id").values + "\","
+        str += "\"to\":\"" + ((js \\ "to" \ "data") (0) \ "id").values + "\","
+        str += "\"message\":\"" + (js \\ "message").values + "\","
+        str += "\"mid\":\"%s\"".format(mid)
+        str += "}"
+        println(str)
+        enrichUser(str)
+      }
+    }
+  }
+
+  def enrichUser(str: String): Unit = {
+    var uri: String = ""
+    val js = parse(str)
+    val from = (js \\ "from").values.toString
+    val to = (js \\ "to").values.toString
+    if (from != appPSID) {
+      uri = s"https://graph.facebook.com/${from}?fields=name,id,gender&access_token=${pageAccessToken}"
+    }
+    else {
+      uri = s"https://graph.facebook.com/${to}?fields=name,id,gender&access_token=${pageAccessToken}"
+    }
+    println(uri)
+    val futureGet = Future {
+      val response: scalaj.http.HttpResponse[String] = scalaj.http.Http(uri).asString
+      response.body
+    }
+    futureGet onComplete {
+      case Success(value) => {
+        println(value)
+      }
+      case Failure(exception) => println(exception)
+    }
+  }
 
 
   val routes: Route =
@@ -58,22 +110,12 @@ object Main extends App with Directives with JsonSupport {
       path("webhook") {
         entity(as[String]) { json =>
           val js = parse(json)
-          println(json)
-          val sender = (js \\ "sender" \ "id").values
-          val mid = "m_" + (js \\ "mid").values
-          println(sender)
+          println(js)
+          val mid = "m_" + (js \\ "mid").values.toString
           println(mid)
-          if (sender != None) {
-            val uri = s"https://graph.facebook.com/${sender}?fields=id,name&access_token=${pageAccessToken}"
-            println(uri)
-            val futureGet = Future {
-              val response: scalaj.http.HttpResponse[String] = scalaj.http.Http(uri).asString
-              response.body
-            }
-            futureGet onComplete {
-              case Success(value) => println(value)
-              case Failure(exception) => println(exception)
-            }
+          if (mid != None) {
+            val str = getMessageInfo(mid)
+//            enrichUser(str)
           }else {
             println("Webhook nao proveniente do messenger")
           }
